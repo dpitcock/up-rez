851c2408d13ab14e45ed11595072217d3f75d708
Dennis Pitcock <dpitcock.dev@gmail.com>
2026-02-05 16:50:42 +0100

    fix: landing page image on prod not showing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'uprez.dpitcock.dev',
+                pathname: '/properties/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3030',
+                pathname: '/properties/**',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
